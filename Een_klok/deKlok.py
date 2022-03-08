9b51560ab6cc3a892e6cf645639bb99ff97e7652
99063871 <99063871@mydavinci.com>
2022-03-08 13:28:15 +0100

    klok gemaakt

---
+++ Een_klok/deKlok.py
@@ -0,0 +1,20 @@
+import tkinter
+from time import strftime
+from turtle import bgcolor
+
+window = tkinter.Tk()
+window.title('Clock')
+window.configure(bg='light blue')
+def time():
+    string = strftime('%H:%M:%S')
+    label.config(text = string)
+    label.after(1000, time)
+label = tkinter.Label(window, 
+            font = ('calibri', 100),
+            foreground = 'black')
+label.configure(bg='light blue')
+label.pack(anchor = 'center')
+
+time()
+ 
+tkinter.mainloop()
