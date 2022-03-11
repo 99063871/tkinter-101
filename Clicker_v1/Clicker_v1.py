790e6d65179ff17ca5fdf69d9d76baba728872ea
99063871 <99063871@mydavinci.com>
2022-03-11 11:08:53 +0100

    knoppen werken alleen styling moet nog verder

--- Clicker_v1/Clicker_v1.py
+++ Clicker_v1/Clicker_v1.py
@@ -1,3 +1,4 @@
+from ctypes import sizeof
 import tkinter
 
 amount = 0
@@ -6,22 +7,32 @@ def commandUp():
     global amount
     amount+=1
     label.configure(text=amount)
+    numberCheck()
 
 def commandDown():
     global amount
     amount-=1
     label.configure(text=amount)
+    numberCheck()
+
 
 window = tkinter.Tk()
-window.geometry("250x100")
-buttonUp = tkinter.Button(text= "Up", padx=100, command=commandUp)
-buttonUp.pack()
+def numberCheck():
+    if amount == 0:
+        window.configure(bg="gray")
+    elif amount >=1:
+        window.configure(bg="green")
+    elif amount <=1:
+        window.configure(bg="red")
+numberCheck()
+buttonUp = tkinter.Button(text= "Up", command=commandUp, width=50)
+buttonUp.pack(ipadx=10, ipady=10, expand=True)
 
-label = tkinter.Label(text=amount, padx=100)
-label.pack()
+label = tkinter.Label(text=amount, width=50)
+label.pack(ipadx=10, ipady=10, expand=True)
 
-buttonDown = tkinter.Button(text= "Down", padx=100, command=commandDown)
-buttonDown.pack()
+buttonDown = tkinter.Button(text= "Down", command=commandDown, width=50)
+buttonDown.pack(ipadx=10, ipady=10, expand=True)
 
 
 window.mainloop()
